423bccdfe6fba9e3e096e2f0715820e877fbfe02
anas <anas.sawalhy@gmail.com>
2025-06-10 10:49:56 +0300

    start 2 problem

---
+++ leetcodeSolutions/Easy/242.ValidAnagram.cs
@@ -0,0 +1,13 @@
+/ Problem: 242.Valid Anagram
+// Link: https://leetcode.com/problems/valid-anagram/
+// Language: C#
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+public class Solution
+{
+	public bool IsAnagram(string s, string t)
+	{
+
+	}
+}
